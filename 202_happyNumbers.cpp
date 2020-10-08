516bd49822b832d043c6940d6a9bd43daa61a8b8
Drumil Patel <drumilpatel720@gmail.com>
2020-10-09 05:03:23 +0530

    Add solution for 202 Happy Numbers
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

---
+++ 202_happyNumbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+private:
+    int numSquareSum(int n) { 
+        int squareSum = 0; 
+        while (n) { 
+            squareSum += (n % 10) * (n % 10); 
+            n /= 10; 
+        } 
+        return squareSum; 
+    } 
+
+public:
+    bool isHappy(int n) {
+        int slow, fast; 
+  
+        slow = fast = n; 
+        do { 
+            slow = numSquareSum(slow); 
+
+            fast = numSquareSum(numSquareSum(fast)); 
+
+        } 
+        while (slow != fast); 
+
+        return (slow == 1); 
+    }
+};
