01c4988c7fb7a48e6e0f96b30f99a250e9d397dd
Bekzhan <bekzhan.kaspakov@nu.edu.kz>
2020-10-19 01:36:20 +0600

    740

---
+++ DeleteAndEarn.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int[] sum = new int[10001];
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum[nums[i]] += nums[i];
+        }
+        
+        for (int i = 2; i < 10001; i++) {
+            sum[i] = Math.max(sum[i-1], sum[i-2]+sum[i]);
+        }
+        
+        return sum[sum.length-1];
+    }
+}
